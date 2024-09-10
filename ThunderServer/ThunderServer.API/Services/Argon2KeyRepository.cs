a043c01394b4ba96df7425d7fc9b660d580cb87c
Zenkraker <guitarforlife2020@gmail.com>
2024-09-10 22:50:11 +0200

    added new argon2KeyRepository

---
+++ ThunderServer/ThunderServer.API/Services/Argon2KeyRepository.cs
@@ -0,0 +1,24 @@
+﻿using ThunderServer.API.Services.Interfaces;
+using ThunderServer.Infrastructure;
+using ThunderServer.Models;
+
+namespace ThunderServer.API.Services;
+
+public class Argon2KeyRepository : IArgon2KeyRepository
+{
+    private readonly ThunderServerContext _context;
+
+    public Argon2KeyRepository(ThunderServerContext context)
+    {
+        this._context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<Argon2Key> AddAsync(Argon2Key argonKey)
+    {
+        await this._context.Argon2Keys.AddAsync(argonKey);
+
+        await this._context.SaveChangesAsync();
+
+        return argonKey;
+    }
+}
---
+++ ThunderServer/ThunderServer.API/Services/Interfaces/IArgon2KeyRepository.cs
@@ -0,0 +1,8 @@
+﻿using ThunderServer.Models;
+
+namespace ThunderServer.API.Services.Interfaces;
+
+public interface IArgon2KeyRepository
+{
+    Task<Argon2Key> AddAsync(Argon2Key argonKey);
+}
