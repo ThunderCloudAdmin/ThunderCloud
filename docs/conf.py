31fb4a3111a351bb56d17cacbb48e1a81fdc5859
ThunderCloudAdmin <thundercloudserver@outlook.com>
2024-09-11 15:44:47 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
 
+# -- Project information
+
+project = 'Thundercloud'
+copyright = '2024, Zenkraker'
+author = 'Zenkraker'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'myst_parser'
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'alabaster'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False
+}
+html_logo = 'img/logo.png'
+
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+# Options for Read the Docs
+html_theme = 'sphinx_rtd_theme'  # Recommended theme for Read the Docs
