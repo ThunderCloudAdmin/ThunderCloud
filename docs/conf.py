4b90e16d894f80c0224ffefd89b30c76d55ff3b1
ThunderCloudAdmin <thundercloudserver@outlook.com>
2024-09-11 15:58:18 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'alabaster'
+html_theme = 'renku'
 html_theme_options = {
     'logo_only': True,
     'display_version': False
